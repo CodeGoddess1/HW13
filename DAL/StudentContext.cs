38ad8a84b547e86d813a575fc5fd95e207917809
CodeGoddess1 <48345161+CodeGoddess1@users.noreply.github.com>
2019-05-24 14:07:59 -0400

    Add files via upload
    something happen to my folders not sure what happened

---
+++ DAL/StudentContext.cs
@@ -0,0 +1,20 @@
+﻿using DataBase_Design.Models;
+using System.Data.Entity;
+
+namespace DataBase_Design.DAL
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext() : base("CollegeStudent")
+        {
+        }
+
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<Score> Scores { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<SCS> SCSs { get; set; }
+        public DbSet <ScoreType> ScoreTypes { get; set; }
+
+        
+    }
+}
